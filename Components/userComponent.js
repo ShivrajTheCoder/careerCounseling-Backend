5cdff1f73c63182468844930d3e0b44d09dceeb7
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-01-05 23:54:05 +0530

    get-details-api-created

--- Components/userComponent.js
+++ Components/userComponent.js
@@ -1,5 +1,6 @@
+const Colleges = require("../Models/Colleges");
+const Courses = require("../Models/Courses");
 const User = require("../Models/User");
-
 const exp=module.exports;
 
 exp.UpdateDetails=async (req,res,next)=>{
@@ -42,6 +43,88 @@ exp.profileDetails=async(req,res,next)=>{
             return res.status(500).json({
                 error,
                 message:"Internal server error"
+            })
+        })
+}
+
+exp.getAllColleges=async (req,res,next)=>{
+    await Colleges.find({})
+    .then(result=>{
+        // console.log(result);
+        if(result.length>0){
+            return res.status(200).json({
+                result
+            })
+        }
+        return res.status(204).json({
+            message:"No college Found"
+        })
+    })
+    .catch(error=>{
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error
+        })
+    })
+ 
+}
+exp.getAllCourses=async (req,res,next)=>{
+    await Courses.find({})
+    .then(result=>{
+        // console.log(result);
+        if(result.length>0){
+            return res.status(200).json({
+                result
+            })
+        }
+        return res.status(204).json({
+            message:"No college Found"
+        })
+    })
+    .catch(error=>{
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error
+        })
+    })
+ 
+}
+
+exp.getCollege=async(req,res,next)=>{
+    const {id}=req.params;
+    await Colleges.findById(id)
+        .then(response=>{
+            if(response.lenght<1){
+                return res.status(404).json({
+                    message:"No college found"
+                })
+            }
+            return res.status(200).json({
+                response
+            })
+        })
+        .catch(error=>{
+            return res.status(500).json({
+                message:"Internal Server Error"
+            })
+        })
+}
+exp.getCourse=async(req,res,next)=>{
+    const {id}=req.params;
+    await Courses.findById(id)
+        .then(response=>{
+            if(response.lenght<1){
+                return res.status(404).json({
+                    message:"No college found"
+                })
+            }
+            return res.status(200).json({
+                response
+            })
+        })
+        .catch(error=>{
+            return res.status(500).json({
+                message:"Internal Server Error"
             })
         })
 }
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,5 +7,14 @@ router.route("/:userId/updateprofile")
 router.route("/:userId/getuserdetails")
     .get(User.profileDetails);
 
-    
+router.route("/getallcolleges")
+    .get(User.getAllColleges);
+
+router.route("/getallcourses")
+    .get(User.getAllCourses);
+
+router.route("/getcollege/:id")
+    .get(User.getCollege);
+router.route("/getcourse/:id")
+    .get(User.getCourse);
 module.exports=router;
