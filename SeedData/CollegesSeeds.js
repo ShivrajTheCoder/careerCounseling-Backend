b0fb03b4eb6d132931d5db6bef74f94975c3ee19
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-01-07 01:45:49 +0530

    data-seeds-added

--- SeedData/CollegesSeeds.js
+++ SeedData/CollegesSeeds.js
@@ -1,5 +1,64 @@
-const collegeData=[
+const { default: mongoose } = require("mongoose");
+const Colleges = require("../Models/Colleges");
+
+main().then(() => {
+    console.log("Connected!!");
+}).catch(err => {
+    console.log(err)
+});
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/careercounseling', { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+const collegeData = [
     {
-        
-    }
+        "name": "Graphic Era",
+        "type": "Private",
+        "description": "The Graphic Era Educational Society (GEES), established in 1993, is a non-profit organization that aims to mobilize world-class education and generate resources for providing and supporting quality education for all. The society recognizes the right of every individual to lead a life of dignity and self-respect in a just and equitable manner. Initially, Graphic Era Educational Society inaugurated its operations as Graphic Era Institute of Technology, which achieved the distinction of being the first self- financed educational institute in North India, offering engineering courses. The Institute was the culmination of the dream of its visionary founder, Prof. (Dr) Kamal Ghanshala, to change the destiny of thousands of youth by providing an excellent and holistic professional education. He had visualized an educational hub that would cater to the academic aspirations of innumerable young men and women and his vision took a concrete shape in the form of Graphic Era Institute.",
+        "location": "Dehradun",
+        "coursesOffered": ["63b87b92ae8e2a18e4f5f997", "63b87b92ae8e2a18e4f5f998",
+            "63b87b92ae8e2a18e4f5f99a", "63b87b92ae8e2a18e4f5f99b", "63b87b92ae8e2a18e4f5f999"]
+    },
+    {
+        "name": "Dehradun Institute of Technology",
+        "type": "Private",
+        "description": "DIT University was founded by Naveen Agarwal in 1998. In 2012, UGC conferred the Autonomous Status to DIT, further in 2013 the Uttarakhand State Government declared DIT as DIT University. DIT University has been established by Govt. of Uttarakhand vide Act No.10 of 2013 dated 15 February 2013 and is recognised by the UGC[4] under section 2(F) of the UGC Act, 1956. DIT University's campus is located on the foothills of Mussoorie. Dehradun is located 240 kilometres north-east of Delhi. The area of the campus is 25 acres out of which 23 acres is developed, the prominent buildings are Vedanta, Chanakya and Civil block. There is a 2 acre ground available for students, parking, and other facilities are also a available in DIT.",
+        "location": "Dehradun",
+        "coursesOffered": ["63b87b92ae8e2a18e4f5f997", "63b87b92ae8e2a18e4f5f998",
+            "63b87b92ae8e2a18e4f5f99a", "63b87b92ae8e2a18e4f5f99b"]
+    },
+    {
+        "name": "Delhi Technological University",
+        "type": "Private",
+        "description": "Formerly known as Delhi College of Engineering, Delhi Technological University (DTU) was established in 1941. The university is accredited by the NAAC with “A” grade and is also approved by the University Grant Commission (UGC). DTU ranked 36 for ‘Engineering’, 42 for ‘University’ and 54 for ‘Overall’ category by the NIRF 2021. DTU is counted amongst the oldest Engineering colleges in India. DTU currently offers more than 80 courses via its 17 departments. Out of these departments, University School of Management and Entrepreneurship (USME) was established recently and is also known as the DTU East Delhi Campus. Additionally, DTU formed Delhi School of Management (DSM) as a department under it to offer courses in the field of Management. All the UG, PG and doctoral programmes available in the field of Engineering, Design, Science and Management, among others, are offered together via DTU’s main campus, DSM and University School of Management & Entrepreneurship (USME).",
+        "location": "Delhi",
+        "coursesOffered": ["63b87b92ae8e2a18e4f5f997", "63b87b92ae8e2a18e4f5f998",
+            "63b87b92ae8e2a18e4f5f99a", "63b87b92ae8e2a18e4f5f99b"]
+    },
+    {
+        "name": "IIT Bombay",
+        "type": "Public",
+        "description": "IIT Bombay was founded in 1958.[11] In 1961, the Parliament decreed IITs as Institutes of National Importance.[12] A committee formed by the Government of India recommended the establishment of four higher institutes of technology to set the direction for the development of technical education in the country in 1946. Planning began in 1957 and the first batch of 100 students was admitted in 1958.[12] Since its establishment in Powai, the institute has physically expanded to include more than 584 major buildings with a combined area of more than 2.2 square kilometers. ",
+        "location": "Mumbai",
+        "coursesOffered": ["63b87b92ae8e2a18e4f5f998"]
+    },
+    {
+        "name": "IIT Kharagpur",
+        "type": "Public",
+        "description": "Indian Institute of Technology Kharagpur (IIT Kharagpur) is a public institute of technology established by the Government of India in Kharagpur, West Bengal, India. Established in 1951, the institute is the first of the IITs to be established and is recognised as an Institute of National Importance. In 2019 it was awarded the status of Institute of Eminence by the Government of India.The institute was initially established to train engineers after India attained independence in 1947. However, over the years, the institute's academic capabilities diversified with offerings in management, law, architecture, humanities, etc. IIT Kharagpur has an 8.7-square-kilometre (2,100-acre) campus and has about 22,000 residents.",
+        "location": "Kharagpur",
+        "coursesOffered": ["63b87b92ae8e2a18e4f5f998"]
+    },
 ]
+
+const uploadColleges=async()=>{
+    await Colleges.deleteMany({});
+
+   await Colleges.insertMany(collegeData);
+    console.log("uploaded")
+}
+
+uploadColleges().then(()=>{
+    mongoose.connection.close();
+});
---
+++ SeedData/ProfessionsSeeds.js
@@ -0,0 +1,51 @@
+const { default: mongoose } = require("mongoose");
+const Professions = require("../Models/Professions");
+
+main().then(() => {
+    console.log("Connected!!");
+}).catch(err => {
+    console.log(err)
+});
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/careercounseling', { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+const professionData=[
+    {
+        "name":"Software Engineer",
+        "img_url":"https://media.istockphoto.com/id/1139938843/photo/programmer-controlling-the-statistics-of-the-site.jpg?b=1&s=612x612&w=0&k=20&c=Kc1UxC8rekuPauQk0D_Q1DkIeYwU73j4IaKcEHc_iQ4=",
+        "description":"Software engineers design and develop computer programs and applications. Strong demand for skilled software engineers means that professionals can pursue many employment options in various industries.",
+        "stream":"science"
+      },
+    {
+        "name":"Doctor",
+        "img_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlyNddILqLUVDyjXHUxOvBPRdoY6-p9ahBsw&usqp=CAU",
+        "description":"A doctor is a professional physician who seeks out various methods and implements them to restore the health of patients. It involves providing surgical, medicinal and therapeutic care to patients.",
+        "stream":"science"
+      },
+    {
+        "name":"Chartered Accountancy",
+        "img_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHasXNmGcVOeJ51f4jpG1PU-_A6SNbrKRqCg&usqp=CAU",
+        "description":"Chartered Accountancy is a professional practice of accounting, auditing, taxation, and financial assessment for an individual or an organisation. Chartered Accountant is a designation given to an accounting professional who has received certification from a statutory body that he/she is qualified to take care of the matters related to accounting and taxation of a business enterprise.",
+        "stream":"commerce"
+      },
+    {
+        "name":"Archeologist",
+        "img_url":"https://scx2.b-cdn.net/gfx/news/2021/chan-chan-was-a-citade.jpg",
+        "description":"They research the invention of the first stone tool to the invention of wheels. In the career as archeologist, one needs to find and analyse objects and structures to inform our understanding of past economic, social, political, and intellectual life. Mostly excavators analyse or plan for research projects to answer questions and test hypotheses about past cultures. ",
+        "stream":"commerce"
+      },
+]
+
+
+const uploadProfession=async()=>{
+    await Professions.deleteMany({});
+
+   await Professions.insertMany(professionData);
+    console.log("uploaded")
+}
+
+uploadProfession().then(()=>{
+    mongoose.connection.close();
+});
